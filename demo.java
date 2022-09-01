11076625dafe7470d099502645b9c9cdaf459643
yamininaveen <105651994+yamininaveen@users.noreply.github.com>
2022-09-01 12:37:30 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo
+{
+           public static void main(String [ ]args)
+          {
+             System.out.println("studyopedia java 18 tutorial");
+             System.out.println("our first java 18 program");
+             System.out.println("neha");
+           }
+
+}
